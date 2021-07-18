06d4499b13ef350094b0dca0bf8ae6b43200175d
StanislavUIT <klibashev.s@beldor.centr.by>
2021-07-18 23:15:08 +0300

    Django 3 / Школа itProger / Программирование - пройдено

--- passport/news/views.py
+++ passport/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 # Create your views here.
@@ -8,6 +9,24 @@ def news_home(request):
     news = Articles.objects.all()
     return render(request, 'news/news.html', {'news': news})
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/detail_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+    # fields = ['troad', 'nroad', 'n_km', 'k_km']
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news-delete.html'
+
+    # form_class = ArticlesForm
+    # fields = ['troad', 'nroad', 'n_km', 'k_km']
 
 def create(request):
     if request.method == 'POST':
